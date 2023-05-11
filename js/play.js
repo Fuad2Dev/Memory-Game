1257672e32b7434f8267b80bce16b16b6483ab10
Fuad-Muhammed <fuad.muhammed@amalitech.com>
2023-05-11 21:36:49 +0000

    solo result page

--- js/play.js
+++ js/play.js
@@ -2,98 +2,106 @@
 let $previous = null;
 
 function move(e) {
-    this.style.background = "#FDA214";
-    let $current = this.childNodes[0];
+  this.style.background = "#FDA214";
+  let $current = this.childNodes[0];
 
-    $current.style.display = "block";
-    this.removeEventListener("click", move);
+  $current.style.display = "block";
+  this.removeEventListener("click", move);
 
-    checkPair($current);
+  checkPair($current);
 }
 
 function checkPair($current) {
-    // if has pair
-    let isFirstPlay = ($previous == null);
+  // if has pair
+  let isFirstPlay = $previous == null;
 
-    if (isFirstPlay) {
-        $previous = $current;
-    } else {
-        let isEqual = ($previous.innerText == $current.innerText);
+  if (isFirstPlay) {
+    $previous = $current;
+  } else {
+    let isEqual = $previous.innerText == $current.innerText;
 
-        if (players > 1) switchTurn(isEqual);
-        else updateMoves();
+    if (players > 1) switchTurn(isEqual);
+    else updateMoves();
 
-        if (isEqual) {
-            //remove click when isEqual
-            $previous.parentNode.removeEventListener("click", move);
-            $current.parentNode.removeEventListener("click", move);
+    if (isEqual) {
+      //remove click when isEqual
+      $previous.parentNode.removeEventListener("click", move);
+      $current.parentNode.removeEventListener("click", move);
 
-            //remove background style
-            $previous.parentNode.style.background = "";
-            $current.parentNode.style.background = "";
-            //add the matched class
-            $previous.parentNode.classList.add("matched");
-            $current.parentNode.classList.add("matched");
-            //check if all the balls have been matched
-            $size = Number(localStorage.getItem("size"));
-            if (document.querySelectorAll(".matched").length == size * size) {
-                //get all the scores in empty array
-                $scores = [];
+      //remove background style
+      $previous.parentNode.style.background = "";
+      $current.parentNode.style.background = "";
+      //add the matched class
+      $previous.parentNode.classList.add("matched");
+      $current.parentNode.classList.add("matched");
+      //check if all the balls have been matched
+      $size = Number(localStorage.getItem("size"));
+      if (document.querySelectorAll(".matched").length == size * size) {
+        
+        if (players > 1) {
+          //get all the scores in empty array
+          $scores = [];
 
-                document.querySelectorAll(".multi-card h3").forEach((element) => {
-                    $scores.push(Number(element.innerText));
-                });
+          document.querySelectorAll(".multi-card h3").forEach((element) => {
+            $scores.push(Number(element.innerText));
+          });
 
-                // get the max score
+          // get the max score
 
-                $max = Math.max(...$scores);
-                //create a result card for each player
-                for (
-                    let index = 0; index < Number(localStorage.getItem("players")); index++
-                ) {
-                    const card = document.createElement("div");
-                    card.setAttribute("class", "flex tile justify-between");
+          $max = Math.max(...$scores);
+          //create a result card for each player
+          for (let index = 0; index < Number(localStorage.getItem("players")); index++) {
+            const card = document.createElement("div");
+            card.setAttribute("class", "flex tile justify-between");
 
-                    const playerDiv = document.createElement("div");
-                    playerDiv.textContent = `Player ${index + 1}`;
+            const playerDiv = document.createElement("div");
+            playerDiv.textContent = `Player ${index + 1}`;
 
-                    const pairsDiv = document.createElement("div");
-                    pairsDiv.textContent = `${$scores[index]} Pairs`;
+            const pairsDiv = document.createElement("div");
+            pairsDiv.textContent = `${$scores[index]} Pairs`;
 
-                    card.appendChild(playerDiv);
-                    card.appendChild(pairsDiv);
+            card.appendChild(playerDiv);
+            card.appendChild(pairsDiv);
 
-                    document.querySelector('#result_cards').appendChild(card);
-                }
-                // show modal
-                document.querySelector(".modal").style.display = "block";
+            document.querySelector("#result_cards").appendChild(card);
+          }
+          // show modal
+          document.querySelector(".modal").style.display = "block";
 
-                //indicate a tie / winner
-                if (hasDuplicateOf($max, $scores))
-                    document.querySelector("#who_won").innerText = `It's a tie!`;
-                else {
-                    document.querySelector(
-                        "#who_won"
-                    ).innerText = `Player ${$scores.indexOf($max) + 1} wins!`;
-                }
+          //indicate a tie / winner
+          if (hasDuplicateOf($max, $scores)) document.querySelector("#who_won").innerText = `It's a tie!`;
+          else {
+            document.querySelector("#who_won").innerText = `Player ${$scores.indexOf($max) + 1} wins!`;
+          }
 
-                //indicate winner / winners
-                document.querySelectorAll(".tile").forEach((element, index) => {
-                    if ($scores[index] == $max)
-                        element.classList.add('winner')
-
-                });
-            }
+          //indicate winner / winners
+          document.querySelectorAll(".tile").forEach((element, index) => {
+            if ($scores[index] == $max) element.classList.add("winner");
+          });
         } else {
-            $previous.parentNode.addEventListener("click", move);
-            $current.parentNode.addEventListener("click", move);
-            delay($previous, $current);
-        }
+            stopTimer()
+          cards = `<div class="flex tile justify-between"><div>Time Elapsed</div><div id="time_elapsed"></div></div>
+            <div class="flex tile justify-between"><div>Moves Taken</div><div id="moves_taken"></div></div>`;
+          document.querySelector("#result_cards").innerHTML = cards;
 
-        $previous = null;
+          document.querySelector("#who_won").innerText = `You did it!`;
+
+          document.querySelector(".modal").style.display = "block";
+          document.querySelector("#time_elapsed").innerText = document.querySelector("#timer").innerText;
+          document.querySelector("#moves_taken").innerText = document.querySelector("#moves").innerText + " Moves";
+          document.querySelector("#subtext").innerText = "Game over! Here's how you got on...";
+        }
+      }
+    } else {
+      $previous.parentNode.addEventListener("click", move);
+      $current.parentNode.addEventListener("click", move);
+      delay($previous, $current);
     }
+
+    $previous = null;
+  }
 }
 
 addEventListener("ball", "click", move);
 
-document.querySelector('#restart').addEventListener('click', )
+document.querySelector("#restart").addEventListener("click");
